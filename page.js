95141def4c86251d9c84c7d3ab5c1adff4350416
GeoffreyOuma <geoffreyouma@students.uonbi.ac.ke>
2022-06-22 21:15:31 +0300

    initial commit

---
+++ page.js
@@ -0,0 +1,8 @@
+// Below is a js code 
+// That prints out my (name, height, and country on the screen)
+
+let myName = "Geofrey Ouma"
+let myHeight = "5 feet"
+let myCountry = "Kenya"
+
+console.log("Hello I am " + myName + " " +  "from " + myCountry + " " + "and I am " + myHeight + " " + "tall.")
